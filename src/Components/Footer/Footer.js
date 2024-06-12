341f87413503a4bc758f26c6a539876b3f7e4bb3
sai1234-tech <saikirankushangala@gmail.com>
2024-06-12 16:42:04 +0530

    worked

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
         <div className='itoconnect_marketsite_footer'>
              <div className='footer_logo_section_left'>
               <div className='itoconnect_log_text'>
-              <img src={ItoCLogo}  className='itoconnect_logo'/>
+              <img src={ItoCLogo}  alt="" className='itoconnect_logo'/>
                    <span className='itoconnect_name'> iToConnect</span>
                    {/* <p className='itoconnect_name'> iToConnect</p> */}
 
@@ -24,14 +24,14 @@ function Footer() {
                       <span className='itoconnect_name_footer_abbrevation'>We built an elegant solution. Our team believe that the success of our company is a result of our clients growth.</span>
                    </div>
                    <div className="social_icons">
-                   <a href="#" class="fa ">
-                            <img src={Twitter}/>
+                   <a href="/" class="fa ">
+                            <img src={Twitter} alt=""/>
                         </a>
-                        <a href="#" class="facebook">
-                            <img src={Facebook}/>
+                        <a href="/" class="facebook">
+                            <img src={Facebook} alt=""/>
                         </a>
-                        <a href="#" class="linkedin">
-                            <img src={LinkedIn}/>
+                        <a href="/" class="linkedin">
+                            <img src={LinkedIn} alt=""/>
                         </a>
                     </div>
              </div>
@@ -63,53 +63,8 @@ function Footer() {
              <span>© 2022. All rights reserved.</span>
        </div>
         </div>
-    {/* <div className='itoconnect_marketsite_footer'>
-             <div className='footer_logo_section_left'>
-                    <img src={ItoCLogo}  className='itoconnect_logo'/>
-                   <span className='itoconnect_name'> iToConnect</span>
-
-                   <div className='itoconnect_name_footer_'>
-                      <span className='itoconnect_name_footer_abbrevation'>We built an elegant solution. Our team believe that the success of our company is a result of our clients growth.</span>
-                   </div>
-                   <div className="social_icons">
-                   <a href="#" class="fa ">
-                            <img src={Twitter}/>
-                        </a>
-                        <a href="#" class="facebook">
-                            <img src={Facebook}/>
-                        </a>
-                        <a href="#" class="linkedin">
-                            <img src={LinkedIn}/>
-                        </a>
-                    </div>
-             </div>
-             <div className='footer_logo_section_right'>
-                      <div >
-                          <ul>
-                            <li><b>iToConnect</b></li>
-                            <li>Get a Live Demo</li>
-                            <li>Conatct Us</li>
-                            <li>Partner with Us</li>
-                            <li>Privacy Policy</li>
-                            <li>Terms & Conditions</li>
-                          </ul>
-                      </div>
-                      <div>
-                          <ul>
-                            <li><b>Quick Help Links</b></li>
-                            <li>Blogs</li>
-                            <li>FAQs</li>
-                            <li>Case Studies</li>
-                            <li>Resource Center</li>
-                            <li>iToConnect Academy</li>
-                          </ul>
-                      </div>
-             </div>
-       </div> */}
-       
-       {/* <div className='itoconnect_footer_bottom_line'>
-             <span>© 2022. All rights reserved.</span>
-       </div> */}
+        
+    
        
     </div>
     
